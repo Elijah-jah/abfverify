1c2e5d842f981b092da0ecde77bfe6bfcec668bf
Elijah <alamuelijah17@gmail.com>
2026-08-04 14:16:03 +0100

    add sitemap.xml and robots.txt for Google indexing

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
 
+
 urlpatterns = [
     path("admin/", admin.site.urls),
 
@@ -19,6 +20,18 @@ urlpatterns = [
     path('googlef0f70b255124184e.html', TemplateView.as_view(
         template_name='googlef0f70b255124184e.html',
         content_type='text/html'
+    )),
+
+    # Sitemap
+    path('sitemap.xml', TemplateView.as_view(
+        template_name='sitemap.xml',
+        content_type='application/xml'
+    )),
+    
+    # Robots.txt
+    path('robots.txt', TemplateView.as_view(
+        template_name='robots.txt',
+        content_type='text/plain'
     )),
     
 ]
