e8cbd0b9d2017d005f973241bfe9de9f6c97438d
Elijah <alamuelijah17@gmail.com>
2026-08-04 14:09:59 +0100

    add Google Search Console HTML verification file

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import include, path
-
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -15,5 +15,10 @@ urlpatterns = [
 
     path("pricing/", include("pricing.urls")),
 
+     # Google Search Console verification
+    path('googlef0f70b255124184e.html', TemplateView.as_view(
+        template_name='googlef0f70b255124184e.html',
+        content_type='text/html'
+    )),
     
 ]
