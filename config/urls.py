c04f28bd8dc55d7a6d265b2bd74b1a1a395445aa
Elijah <alamuelijah17@gmail.com>
2026-08-04 14:25:31 +0100

    hide admin panel at custom URL

--- config/urls.py
+++ config/urls.py
@@ -4,7 +4,7 @@ from django.views.generic import TemplateView
 
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('semilore/', admin.site.urls),
 
     path("", include("website.urls")),
 
