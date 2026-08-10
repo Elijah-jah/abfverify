9db62832ec43858282f724f69eaa3c235e5e3139
Elijah <alamuelijah17@gmail.com>
2026-08-10 11:33:37 +0100

    add Brevo email settings

--- config/settings.py
+++ config/settings.py
@@ -168,6 +168,14 @@ PAYSTACK_SECRET_KEY = config("PAYSTACK_SECRET_KEY")
 PAYSTACK_CALLBACK_URL = config("PAYSTACK_CALLBACK_URL")
 
 
+# Email Settings
+EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.console.EmailBackend')
+EMAIL_HOST = config('EMAIL_HOST', default='')
+EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='webmaster@localhost')
 
 
 # Security settings
